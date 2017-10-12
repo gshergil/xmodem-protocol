672a01e933145ddfe450b8497a864740975e33a8
Lui <injabie3@gmail.com>
2017-10-12 16:50:54 -0700

    [myIO] First rendition of Part 3.

--- Ensc351Part2/src/myIO.cpp
+++ Ensc351Part2/src/myIO.cpp
@@ -8,10 +8,35 @@
 #include <fcntl.h>			// for open/creat
 #include <sys/socket.h> 		// for socketpair
 #include "SocketReadcond.h"
+#include <mutex>
+#include <condition_variable>
+#include <vector>
+
+using namespace std;
+
+struct myCondition
+{
+    mutex m;
+    condition_variable cV;
+    int buffered = 0;
+    int pairNum = 0; // Your other socket pair's descriptor.
+};
+
+myCondition oneDBuffer[10]; // Array of one directional "buffer" objects.
+
+int myReadcond(int des, void * buf, int n, int min, int time, int timeout);
 
 int mySocketpair( int domain, int type, int protocol, int des[2] )
 {
 	int returnVal = socketpair(domain, type, protocol, des);
+    
+    // Create the objects, and make it so each direction knows its other direction pair.
+    //oneDBuffer[des[0]] = myCondition;
+    oneDBuffer[des[0]].pairNum = des[1]; 
+    
+    //oneDBuffer[des[1]] = myCondition;
+    oneDBuffer[des[1]].pairNum = des[0];
+    
 	return returnVal;
 }
 
@@ -27,12 +52,25 @@ int myCreat(const char *pathname, mode_t mode)
 
 ssize_t myRead( int fildes, void* buf, size_t nbyte )
 {
-	return read(fildes, buf, nbyte );
+	return myReadcond(fildes, buf, nbyte, 1, 0, 0);
+    //return read(fildes, buf, nbyte );
 }
 
 ssize_t myWrite( int fildes, const void* buf, size_t nbyte )
 {
-	return write(fildes, buf, nbyte );
+    int bytesWritten = 0;
+    
+    // Step 1. Acquire the lock.
+    lock_guard<mutex> lgLock(oneDBuffer[fildes].m);
+
+    // Step 2. Do the write.
+	bytesWritten = write(fildes, buf, nbyte );
+    
+    // Step 3. Increment the buffer counter.
+    oneDBuffer[fildes].buffered += bytesWritten;
+    
+    // Step 4/5. Return and unlock (on out of scope).
+    return bytesWritten;
 }
 
 int myClose( int fd )
@@ -42,11 +80,39 @@ int myClose( int fd )
 
 int myTcdrain(int des)
 { //is also included for purposes of the course.
-	return 0;
+    // Step 1. Acquire the lock. Make sure to use unique 
+    // because we have condition variable.
+    unique_lock<mutex> uLock(oneDBuffer[des].m);
+    
+    // Step 2. Wait for the condition variable.
+    oneDBuffer[des].cV.wait(uLock, [des] { return oneDBuffer[des].buffered == 0; });
+    
+    // Step 3. Unlock and continue your way.
+   	return 0;
 }
 
 int myReadcond(int des, void * buf, int n, int min, int time, int timeout)
 {
-	return wcsReadcond(des, buf, n, min, time, timeout );
+    // Step 1. Lock the mutex.  Make sure you lock the right one.
+    // Get the other descriptor
+    int fildes = oneDBuffer[des].pairNum;
+    int bytesRead = 0;
+    
+    lock_guard<mutex> lgLock(oneDBuffer[fildes].m);
+    
+    // Step 2. Do the read.
+    bytesRead = wcsReadcond(des, buf, n, min, time, timeout );
+    
+    // Step 3. Decrement the buffer counter.
+    oneDBuffer[fildes].buffered -= bytesRead;
+    
+    // Step 4. If it's 0, notify your other thread.
+    if (oneDBuffer[fildes].buffered == 0)
+    {
+        oneDBuffer[fildes].cV.notify_one();
+    }
+    
+    // Step 5. Return and unlock (on out of scope)
+    return bytesRead;
 }
 
