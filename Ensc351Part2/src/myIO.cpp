23fc2e0d0b2249e93a1a0a7c2f64c68cd4ba9b8d
Lui <injabie3@gmail.com>
2017-10-20 23:15:44 -0700

    [myIO] Add support for dynamic allocation.

--- Ensc351Part2/src/myIO.cpp
+++ Ensc351Part2/src/myIO.cpp
@@ -17,6 +17,9 @@
 // that you used in making your submission.
 //
 // Resources: None
+//
+// Contains commented out static array version.
+//
 
 /* Wrapper functions for ENSC-351, Simon Fraser University, By
  *  - Craig Scratchley
@@ -40,7 +43,11 @@ struct myCondition
     condition_variable cV;
     int buffered = 0;
     int pairNum = 0; // Your other socket pair's descriptor.
+    bool isSocket = true;
 };
+
+vector<myCondition*> myVector;
+mutex vectorMutex;
 
 myCondition oneDBuffer[10]; // Array of one directional "buffer" objects.
 
@@ -49,25 +56,70 @@ int myReadcond(int des, void * buf, int n, int min, int time, int timeout);
 int mySocketpair( int domain, int type, int protocol, int des[2] )
 {
 	int returnVal = socketpair(domain, type, protocol, des);
+    int largeDes = 0;
+    
+    // Lock the vector.
+    lock_guard<mutex> lk(vectorMutex);
+    
+    // Get the largest descriptor.
+    if (des[0] > des[1])
+        largeDes = des[0];
+    else
+        largeDes = des[1];
+    
+    // Resize the vector if it's not large enough.
+    if (myVector.size() < (unsigned)(largeDes + 1))
+        myVector.resize(largeDes + 1);
     
     // Create the objects, and make it so each direction knows its other direction pair.
+    myVector[des[0]] = new myCondition;
+    myVector[des[1]] = new myCondition;
+    
     //oneDBuffer[des[0]] = myCondition;
-    oneDBuffer[des[0]].pairNum = des[1]; 
+    //oneDBuffer[des[0]].pairNum = des[1]; 
+    myVector[des[0]]->pairNum = des[1];
     
     //oneDBuffer[des[1]] = myCondition;
-    oneDBuffer[des[1]].pairNum = des[0];
+    //oneDBuffer[des[1]].pairNum = des[0];
+    myVector[des[1]]->pairNum = des[0];
     
 	return returnVal;
 }
 
 int myOpen(const char *pathname, int flags, mode_t mode)
 {
-	return open(pathname, flags, mode);
+    int des = open(pathname, flags, mode);
+    
+    //1. Obtain vector mutex
+    lock_guard<mutex> lgLock(vectorMutex);
+    
+    // Resize the vector if needed.
+    if (myVector.size() < (unsigned)(des + 1))
+        myVector.resize(des + 1);
+    
+    // Create object, but specify that it is not a socket.
+    myVector[des] = new myCondition;
+    myVector[des]->isSocket = false;
+    
+    return des;
 }
 
 int myCreat(const char *pathname, mode_t mode)
 {
-	return creat(pathname, mode);
+	int des = creat(pathname, mode);
+    
+    //1. Obtain vector mutex
+    lock_guard<mutex> lgLock(vectorMutex);
+    
+    // Resize the vector if needed.
+    if (myVector.size() < (unsigned)(des + 1))
+        myVector.resize(des + 1);
+    
+    // Create object, but specify that it is not a socket.
+    myVector[des] = new myCondition;
+    myVector[des]->isSocket = false;
+    
+    return des;
 }
 
 ssize_t myRead( int fildes, void* buf, size_t nbyte )
@@ -76,22 +128,30 @@ ssize_t myRead( int fildes, void* buf, size_t nbyte )
     //return read(fildes, buf, nbyte );
 
 	// Get the other descriptor
-	int otherDescriptor = oneDBuffer[fildes].pairNum;
+	//int otherDescriptor = oneDBuffer[fildes].pairNum;
+    int otherDescriptor = myVector[fildes]->pairNum;
+    
 	int bytesRead = 0;
 
 	// Step 1. Do the read from the originating socket first, and store bytes read.
 	bytesRead = read(fildes, buf, nbyte);
 
 	// Step 2. Lock the mutex.  Make sure you lock the right one.
-	lock_guard<mutex> lgLock(oneDBuffer[otherDescriptor].m);
+	//lock_guard<mutex> lgLock(oneDBuffer[otherDescriptor].m);
+    lock_guard<mutex> lgLock(myVector[otherDescriptor]->m);
 
 	// Step 3. Decrement the buffer counter.
-	oneDBuffer[otherDescriptor].buffered -= bytesRead;
+	//oneDBuffer[otherDescriptor].buffered -= bytesRead;
+    myVector[otherDescriptor]->buffered -= bytesRead;
 
 	// Step 4. If it's 0, notify your other thread.
-	if (oneDBuffer[otherDescriptor].buffered == 0)
+	// if (oneDBuffer[otherDescriptor].buffered == 0)
+	// {
+	//	oneDBuffer[otherDescriptor].cV.notify_one();
+	// }
+    if (myVector[otherDescriptor]->buffered == 0)
 	{
-		oneDBuffer[otherDescriptor].cV.notify_one();
+		myVector[otherDescriptor]->cV.notify_one();
 	}
 
 	// Step 5. Return and unlock (on out of scope)
@@ -103,13 +163,15 @@ ssize_t myWrite( int fildes, const void* buf, size_t nbyte )
     int bytesWritten = 0;
     
     // Step 1. Acquire the lock.
-    lock_guard<mutex> lgLock(oneDBuffer[fildes].m);
+    //lock_guard<mutex> lgLock(oneDBuffer[fildes].m);
+    lock_guard<mutex> lgLock(myVector[fildes]->m);
 
     // Step 2. Do the write.
 	bytesWritten = write(fildes, buf, nbyte );
     
     // Step 3. Increment the buffer counter.
-    oneDBuffer[fildes].buffered += bytesWritten;
+    //oneDBuffer[fildes].buffered += bytesWritten;
+	myVector[fildes]->buffered += bytesWritten;
     
     // Step 4/5. Return and unlock (on out of scope).
     return bytesWritten;
@@ -117,17 +179,58 @@ ssize_t myWrite( int fildes, const void* buf, size_t nbyte )
 
 int myClose( int fd )
 {
-	return close(fd);
+    int des = close(fd);
+    int otherDescriptor = 0;
+    
+    unique_lock<mutex> ulVector(vectorMutex);
+    
+    if (!(myVector[fd]->isSocket))
+    {
+        delete myVector[fd];
+        return des;
+    }
+    else
+    {
+        // Get two mutexes.
+        //unique_lock<mutex> ulCurrentFD(myVector[des]->m, defer_lock);
+        //otherDescriptor = myVector[fd]->pairNum;
+        //unique_lock<mutex> ulOtherFD(myVector[otherDescriptor]->m, defer_lock);
+        
+        // Lock both at the same time.
+        //lock(ulCurrentFD, ulOtherFD);
+        //ulCurrentFD.lock();
+        
+        // Notify all other threads.
+        //myVector[des]->cV.notify_all();
+        //myVector[otherDescriptor]->cV.notify_all();
+        
+        // Unlock so other threads can wake up successfully.
+        //ulCurrentFD.unlock();
+        //ulOtherFD.unlock();
+        
+        // Relock and delete.
+        //lock(ulCurrentFD, ulOtherFD);
+        //ulCurrentFD.lock();
+        
+        delete myVector[des];
+        myVector[des] = NULL;
+        delete myVector[otherDescriptor];
+        myVector[otherDescriptor] = NULL;
+        
+        return des;
+    }
 }
 
 int myTcdrain(int des)
 { //is also included for purposes of the course.
     // Step 1. Acquire the lock. Make sure to use unique 
     // because we have condition variable.
-    unique_lock<mutex> uLock(oneDBuffer[des].m);
+    //unique_lock<mutex> uLock(oneDBuffer[des].m);
+    unique_lock<mutex> uLock(myVector[des]->m);
     
     // Step 2. Wait for the condition variable.
-    oneDBuffer[des].cV.wait(uLock, [des] { return oneDBuffer[des].buffered == 0; });
+    //oneDBuffer[des].cV.wait(uLock, [des] { return oneDBuffer[des].buffered == 0; });
+    myVector[des]->cV.wait(uLock, [des] { return myVector[des]->buffered == 0; });
     
     // Step 3. Unlock and continue your way.
    	return 0;
@@ -137,22 +240,29 @@ int myReadcond(int des, void * buf, int n, int min, int time, int timeout)
 {
 
     // Get the other descriptor
-    int fildes = oneDBuffer[des].pairNum;
+    //int fildes = oneDBuffer[des].pairNum;
+    int fildes = myVector[des]->pairNum;
     int bytesRead = 0;
 
     // Step 1. Do the read first from the originating socket, because wcsReadcond can block, and store bytes read.
     bytesRead = wcsReadcond(des, buf, n, min, time, timeout );
     
     // Step 2. Lock the mutex.  Make sure you lock the right one.
-    lock_guard<mutex> lgLock(oneDBuffer[fildes].m);
+    //lock_guard<mutex> lgLock(oneDBuffer[fildes].m);
+    lock_guard<mutex> lgLock(myVector[fildes]->m);
     
     // Step 3. Decrement the buffer counter.
-    oneDBuffer[fildes].buffered -= bytesRead;
+    //oneDBuffer[fildes].buffered -= bytesRead;
+    myVector[fildes]->buffered -= bytesRead;
     
     // Step 4. If it's 0, notify your other thread.
-    if (oneDBuffer[fildes].buffered == 0)
+    //if (oneDBuffer[fildes].buffered == 0)
+    //{
+    //    oneDBuffer[fildes].cV.notify_one();
+    //}
+    if (myVector[fildes]->buffered == 0)
     {
-        oneDBuffer[fildes].cV.notify_one();
+        myVector[fildes]->cV.notify_one();
     }
     
     // Step 5. Return and unlock (on out of scope)
